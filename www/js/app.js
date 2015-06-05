d349c173a3117976d7065705e156abe8d61e1928
Liu Lixiang <liulixiang1988@gmail.com>
2015-06-05 23:43:21 +0800

    infinite scroll and pull to refresh

--- www/js/app.js
+++ www/js/app.js
@@ -4,12 +4,36 @@ app = angular.module('reader', ['ionic', 'angularMoment']);
 
 app.controller('RedditCtrl',function($http, $scope){
   $scope.stories = [];
-  $http.get("http://www.reddit.com/r/Android/.json")
-    .success(function(response){
-      angular.forEach(response.data.children, function(child){
-        $scope.stories.push(child.data);
+
+  function loadStories(params, callback){
+    var stories = []
+    $http.get("http://www.reddit.com/r/funny/new/.json", {params: params})
+      .success(function(response){
+        angular.forEach(response.data.children, function(child){
+          stories.push(child.data);
+        });
+        callback(stories);
       });
+  }
+
+  $scope.loadOlderStories = function(){
+    var params = {};
+    if($scope.stories.length > 0){
+      params['after'] = $scope.stories[$scope.stories.length-1].name;
+    }
+    loadStories(params, function(oldStories){
+      $scope.stories = $scope.stories.concat(oldStories);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
     });
+  }
+
+  $scope.loadNewerStories = function(){
+    var params = {'before': $scope.stories[0].name};
+    loadStories(params, function(newerStories){
+      $scope.stories = newerStories.concat($scope.stories);
+      $scope.$broadcast('scroll.refreshComplete');
+    })
+  }
 });
 
 app.run(function($ionicPlatform, amMoment) {
