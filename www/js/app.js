94795d61ef2a2cf6ee4247471d78eaf4f5c3d68f
Liu Lixiang <liulixiang1988@gmail.com>
2015-06-05 22:16:41 +0800

    init the app reader

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+(function(){
+
+app = angular.module('reader', ['ionic']);
+
+app.controller('RedditCtrl',function($http, $scope){
+  $scope.stories = [];
+  $http.get("http://www.reddit.com/r/Android/.json")
+    .success(function(response){
+      angular.forEach(response.data.children, function(child){
+        $scope.stories.push(child.data);
+      });
+    });
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+}());
